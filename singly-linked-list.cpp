92b0c3c39e6b9001991f1207a55e99791d472097
Bruno Alfred Mkonyi <alfredybruno@gmail.com>
2021-01-19 23:45:57 +0300

    Showing the Data in the nodes created

--- singly-linked-list.cpp
+++ singly-linked-list.cpp
@@ -14,5 +14,37 @@ int main(int argc, char const *argv[])
 {
 	/* code */
 	
+	// Creating pointer for our nodes
+	Node* head = NULL;
+	Node* second = NULL;
+	Node* third = NULL;
+
+	// Allocating the heap from the memory
+
+	head = new Node();
+	second = new Node();
+	third = new Node();
+
+	//Creating first Node
+	head -> data = 1;
+	head -> next = second;
+
+	//Creating second Node
+	second -> data = 2;
+	second -> next = third;
+
+	//Creating third Node
+	third -> data = 3;
+	third -> next = NULL;
+
+	cout << "First Node \n" << head -> data << endl;
+	cout << head->next << endl;
+
+	cout << "Second Node \n" << second->data << endl;
+	cout << second->next << endl;
+
+	cout << "Third Node \n" << third -> data << endl;
+	cout << third->next << endl;
+	
 	return 0;
 }
