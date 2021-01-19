cafe6de4b0c6de30d587b5543d95616ed169b70b
Bruno Alfred Mkonyi <alfredybruno@gmail.com>
2021-01-20 01:02:11 +0300

    Creating Addition of a value at the begging of LinkedList

---
+++ LinkedList/singly-linked-list.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+	int data;
+	Node * next;
+
+
+};
+
+// Writing a Functin to print content of Linked List
+void printList(Node *n)
+{
+	while (n != NULL)
+	{
+		/* code */
+		cout << n->data << "\n";
+		n = n->next;
+	}
+}
+
+
+// Inserting a new Node to the Linked List
+void push(Node** head_ref, int new_data){
+
+	// 1.Create an new Node
+	Node* new_node = new Node();
+	// 2.Store data to the Node
+	new_node -> data = new_data;
+	// 3.Make the next of new_node have address of previous head
+	new_node -> next = (*head_ref);
+	// 4.move the head to point to the new node */
+	(*head_ref) = new_node;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	
+	// Creating pointer for our nodes
+	Node* head = NULL;
+	Node* second = NULL;
+	Node* third = NULL;
+
+	// Allocating the heap from the memory
+	head = new Node();
+	second = new Node();
+	third = new Node();
+
+	//Creating first Node
+	head -> data = 1;
+	head -> next = second;
+
+	//Creating second Node
+	second -> data = 2;
+	second -> next = third;
+
+	//Creating third Node
+	third -> data = 3;
+	third -> next = NULL;
+
+	//  Added to head of Linked List
+	push(&head, 5);
+	printList(head);
+	
+
+	return 0;
+
+}
