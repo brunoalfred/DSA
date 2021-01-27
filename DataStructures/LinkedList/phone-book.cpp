4045542c8546bd3d2120260ad3ae30c21c7803fd
Bruno Alfred Mkonyi <alfredybruno@gmail.com>
2021-01-27 15:16:04 +0300

    Added an implementation for phone book with linked list

---
+++ DataStructures/LinkedList/phone-book.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#incl
+using namespace std;
+
+
+class Contact {
+    public:
+        string first_name;
+        string last_name;
+        string phone;
+        string gender;
+       
+    Contact * next;
+    
+    
+}
+
+
+int main(){
+
+}
