40e649b162943636a8d9493388a141d712280e29
Bruno Alfred Mkonyi <alfredybruno@gmail.com>
2021-02-24 12:20:03 +0300

    Creating a Stack and Queue DS with array.

---
+++ DataStructures/Stack/ArrayStack/cmake-build-debug/CMakeFiles/ArrayStack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "ArrayStack"
+  "ArrayStack.pdb"
+  "CMakeFiles/ArrayStack.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ArrayStack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
